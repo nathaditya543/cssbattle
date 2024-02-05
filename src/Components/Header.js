9769e1624e805f8be1c810e76d539b4a0d4986d1
nathaditya <nathaditya@outlook.com>
2024-02-05 11:31:46 +0530

    fixed bug regardind small screen header centering

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import logo from '../CSSBattleImages/iSpaceLogo.png'
+
 import "./style.css"
 
 export default function Header() {
   return (
     <div className='headerContainer'>
         <div className = "header large">
-            <span></span>
-            <span style={{fontSize:"0.8em"}}>VITC-EVENT</span>
+            <span style={{fontSize:"0.5em"}}>Welcome to</span>
             <br></br>
-              <img src = {logo} style={{transform:"scale(0.8)"}}></img>
+            CSS BATTLE 1.0
             <br></br>
-            <span style={{fontSize:"0.8em"}}>Eve@$0502</span>
+            <span style={{fontSize:"0.5em"}}>by iSpace!</span>
         </div>
     </div>
   )
